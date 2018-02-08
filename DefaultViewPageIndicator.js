af203bb724145963a4f946acf630eb2fc22cca3f
Sujay Jaju <sujay@jajus.com>
2018-02-08 13:13:33 +0530

    PropTypes and React.createClass deprecation resolution

--- DefaultViewPageIndicator.js
+++ DefaultViewPageIndicator.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var React = require('react');
+var PropTypes = require('prop-types');
 var ReactNative = require('react-native');
+var createReactClass = require('create-react-class');
 var {
   Dimensions,
   StyleSheet,
@@ -46,11 +48,11 @@ var styles = StyleSheet.create({
   },
 });
 
-var DefaultViewPageIndicator = React.createClass({
+var DefaultViewPageIndicator = createReactClass({
   propTypes: {
-    goToPage: React.PropTypes.func,
-    activePage: React.PropTypes.number,
-    pageCount: React.PropTypes.number
+    goToPage: PropTypes.func,
+    activePage: PropTypes.number,
+    pageCount: PropTypes.number
   },
 
   getInitialState() {
